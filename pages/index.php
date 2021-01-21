cd12c799a49fb9a9eecb140c5414ee57380e70ed
Jan Kristinus <jan.kristinus@yakamara.de>
2021-01-21 21:21:42 +0100

    title changed

--- pages/index.php
+++ pages/index.php
@@ -3,7 +3,7 @@
 /** @var rex_addon $this */
 
 // Die Subpages müssen dem Titel nicht mehr übergeben werden
-echo rex_view::title(rex_i18n::msg('simple_oauth_title'));
+echo rex_view::title(rex_i18n::msg('simple_saml_title'));
 
 // Subpages können über diese Methode eingebunden werden. So ist sichergestellt, dass auch Subpages funktionieren,
 // die von anderen Addons/Plugins hinzugefügt wurden
