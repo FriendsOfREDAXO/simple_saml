8daffe2048f1e4303d5537d226832665ce43b906
Jan Kristinus <jan.kristinus@yakamara.de>
2021-01-21 21:24:32 +0100

    comments deleted in boot

--- boot.php
+++ boot.php
@@ -1,17 +1,5 @@
 <?php
 
-// TODOS:
-// Cert vom SP testen beachten ? .. Damit das auch klappt ..
-// Verwaltung bauen oder doch lieber programatisch ?
-// Logout Link bauen (mit returnTo und ohne??)
-// NameIDFormat aus Metas noch beachtem
-// Active Directory Federation Services (ADFS)
-// - am Beispiel Nextcloud: https://portal.nextcloud.com/article/configuring-single-sign-on-10.html
-
-// SSO auch mit entityKey
-// SLO auch einbauen.
-// Metadata anzeigen - Info ob private und certificate matchen
-
 if (rex::isFrontend()) {
     \REDAXO\Simple_SAML\Modules\Modules::setModule(new \REDAXO\Simple_SAML\Modules\YCom());
 
